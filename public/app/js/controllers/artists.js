256e548591e7e36ffb0095aa84b61e709b9318d0
Roy Smith <roy.smith0820@gmail.com>
2017-02-09 19:52:42 -0500

    delete js files and find api

--- public/app/js/controllers/artists.js
+++
@@ -1,6 +0,0 @@
-app.controller('ArtistsCtrl', ['$scope', '$http',
-  function ($scope, $http) {
-    $http.get('data/mus-artists.json').success(function(data) {
-      $scope.artists = data;
-    });
-  }]);
--- public/app/js/controllers/playlists.js
+++
@@ -1,6 +0,0 @@
-app.controller('PlaylistsCtrl', ['$scope', '$http',
-  function ($scope, $http) {
-    $http.get('data/mus-playlists.json').success(function(data) {
-      $scope.playlists = data;
-    });
-  }]);
